178643d1f615a1a7531c61c5e55c26667bb1d43c
“John <jpiscani@falcon.bentley.edu>
2023-04-05 13:05:20 -0400

    updated with alert dialog and shopping list xmls

--- app/src/main/java/com/example/mealplanner/MainActivity.java
+++ app/src/main/java/com/example/mealplanner/MainActivity.java
@@ -9,10 +9,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //Test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Test comment
     }
 
     @Override
